3d8002223f9b02922658dd76a4a7ec02b73426bd
jenny <niezhe95@qq.com>
2021-05-12 15:27:41 +0800

    退出登录

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .forms import UserLoginForm
 
@@ -27,3 +27,9 @@ def user_login(request):
         return render(request, 'userprofile/login.html', context)
     else:
         return HttpResponse('请求格式不对')
+
+
+# 退出登录
+def user_logout(request):
+    logout(request)
+    return redirect('article:article_list')
